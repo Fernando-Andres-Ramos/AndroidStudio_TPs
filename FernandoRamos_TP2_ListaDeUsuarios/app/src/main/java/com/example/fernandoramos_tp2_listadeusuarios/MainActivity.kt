4912eb7a27382e7f653479e238489527da60a3e9
Fernando Ramos <fernando-ramos94@hotmail.com>
2022-09-22 20:05:08 -0300

    proyecto de TP2 creado junto con archivo .gitignore

---
+++ FernandoRamos_TP2_ListaDeUsuarios/app/src/main/java/com/example/fernandoramos_tp2_listadeusuarios/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fernandoramos_tp2_listadeusuarios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
