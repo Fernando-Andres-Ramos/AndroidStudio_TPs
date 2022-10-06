74dfed165bbd2c0f3d6c478dd71f5b472736df63
Fernando Ramos <fernando-ramos94@hotmail.com>
2022-10-06 20:24:28 -0300

    cambios de estilo en la interfaz

--- FernandoRamos_TP2_ListaDeUsuarios/app/src/main/java/com/example/fernandoramos_tp2_listadeusuarios/UsersDB.kt
+++ FernandoRamos_TP2_ListaDeUsuarios/app/src/main/java/com/example/fernandoramos_tp2_listadeusuarios/UsersDB.kt
@@ -11,60 +11,6 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-/*  companion object{
-      private val users = arrayOf(
-          User(
-              "1",
-              "Fernando",
-              "27", "Argentina",
-              "https://viajes.nationalgeographic.com.es/medio/2018/02/27/hemis-0548910__800x800.jpg",
-              "emailFer",
-              "123456789",
-              "callefalsa123"),
-          User(
-              "2",
-              "Vera",
-              "35",
-              "Nicaragua",
-              "https://thumbs.dreamstime.com/b/hermoso-paisaje-de-monta%C3%B1a-verano-vastos-prados-y-bosques-214333311.jpg",
-              "emailVera",
-              "987654321",
-              "Otracalle321"),
-          User(
-              "3",
-              "Mauro",
-              "28",
-              "Argentina",
-              "https://viajes.nationalgeographic.com.es/medio/2018/02/27/hemis-0255544__800x800.jpg",
-              "emailMauro",
-              "123456",
-              "tokio"),
-          User(
-              "4",
-              "Ayelen",
-              "29","Argentina",
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqrd4qx718rIBlZ5ZMlqWZl6vfK8SmQzMMLcp-V9RQ9ZJqCxGYlGtVjyMYCwAoEG3Up-M&usqp=CAU",
-              "emailAye",
-              "1234589",
-              "Canada"),
-          User(
-              "5",
-              "Patricia",
-              "52",
-              "Argentina",
-              "https://i1.adis.ws/i/canon/pro-best-mirrorless-kit-landscapes-1-1x1_3e52b8e3302a457181059b0a67ecc92b",
-              "emailMama",
-              "12333333",
-              "garcia merou"),
-          User("6",
-              "Tatiana",
-              "27",
-              "Argentina",
-              "Algo",
-              "emailTaty",
-              "987321",
-              "algun lado")
-      )*/
 
 
 /* Repositorio de datos mockeados. */
@@ -81,7 +27,7 @@ class UsersDB (context: Context){
 
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://randomuser.me/")
-            .addConverterFactory(MoshiConverterFactory.create(moshi).asLenient())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
 
         serviceAPI = retrofit.create(ServiceRandomUser::class.java)
@@ -132,7 +78,6 @@ class UsersDB (context: Context){
             }
         })
     }
-
 
     fun findUserByID(id:String?):User?{
         return null
